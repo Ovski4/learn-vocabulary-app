747fd54b8acc0bb5bbda07be2c1943f61aab40d0
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2018-11-17 23:19:47 +1300

    Toujours plus de tests

--- tests/components/settings/SettingsScreen.test.js
+++ tests/components/settings/SettingsScreen.test.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import SettingsScreen from '../../../src/components/settings/settingsScreen/component';
+import { createStore } from 'redux';
+import reducer from '../../../src/reducers/index';
+import SettingsScreen from '../../../src/components/settings/settingsScreen/connect';
 import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux'
 
 describe('SettingsScreen component', () => {
 
-    it('Should render the settings screen without crashing with english locale', async (done) => {
-        const rendered = (await renderer.create(<SettingsScreen locale="en" />)).toJSON();
+    const getStore = (locale) => {
+        return createStore(reducer, {
+            config: {
+                locale: locale
+            }
+        });
+    };
+
+    it('Should render the settings screen with the english locale', () => {
+        const rendered = renderer.create(
+            <Provider store={getStore('en')}>
+                <SettingsScreen />
+            </Provider>).toJSON();
+
         expect(rendered).toMatchSnapshot();
-        done();
     });
 
-    it('Should render the settings screen without crashing with french locale', async (done) => {
-        const rendered = (await renderer.create(<SettingsScreen locale="fr" />)).toJSON();
+    it('Should render the settings screen with the french locale', () => {
+        const rendered = renderer.create(
+            <Provider store={getStore('fr')}>
+                <SettingsScreen />
+            </Provider>).toJSON();
+
         expect(rendered).toMatchSnapshot();
-        done();
     });
 
 });
